8f01071e4f8747d6132197796d1e5cadf3e85fc6
MarlonCJ <marlon.edinsoncj@gmail.com>
2026-01-18 16:42:10 -0500

    1. archivo principal

---
+++ plugin-whatsapp.php
@@ -0,0 +1,12 @@
+<?php 
+
+/**
+ * Plugin Name: Plugin WhatsApp
+ * Description: Botón Flotante de WhatsApp personalizable
+ * Version:1.0.0
+ * Autor: Marlon Jara
+ */
+
+
+defined('ABSPATH') || exit;
+
